e545678a68080e716e26b600809722af6c80f2a8
Fulcrum <sokatanocw@gmail.com>
2022-06-13 19:16:30 -0700

    3rd commit

---
+++ src/data.js
@@ -0,0 +1,19 @@
+const people = [
+  {
+    id: 1,
+    image: '../Images/App_Store_1@4x.png',
+    title: 'The Professional',
+  },
+  {
+    id: 2,
+    image: '../Images/Apple_TV_1@4x.png',
+    title: 'The Digital Creator',
+  },
+  {
+    id: 3,
+    image: '../Images/Audible_1@4x.png',
+    title: 'The Performer',
+  },
+];
+
+export default people;
