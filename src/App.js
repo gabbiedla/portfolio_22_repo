ccb189ee53cba9c604e558f210178506cd4e8b74
Fulcrum <sokatanocw@gmail.com>
2022-06-21 16:32:52 -0700

    4th commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,23 @@ import data from './data';
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  });
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 5000);
+    return () => clearInterval(slider);
+  }, [index]);
   return (
     <section className="section">
       <div className="title">
@@ -15,21 +32,39 @@ function App() {
       </div>
       <div className="section-center">
         {people.map((person, personIndex) => {
-          const { id, image, title } = person;
+          const { id, image, name, title } = person;
           //more stuff coming up
+          let position = 'nextSlide';
+          if (personIndex === index) {
+            position = 'activeSlide';
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = 'lastSlide';
+          }
           return (
-            <article key={id}>
-              <img src={image} alt={title} className="person-img" />
-              <h4>{title}</h4>
+            <article className={position} key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title"> {title}</p>
+              {/* <p className="text"> {quote}</p> */}
+              {/* <FaQuoteRight className="icon" /> */}
             </article>
           );
         })}
-        <button className="prev">
+        <button className="prev" onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
-        <button className="next">
+        <button className="next" onClick={() => setIndex(index + 1)}>
           <FiChevronRight />
         </button>
+        <div className="dots-container">
+          <span className="dots" onClick={() => setIndex(index - 1)}></span>
+          <span className="dots" onClick={() => setIndex(index - 1)}></span>
+          <span className="dots" onClick={() => setIndex(index - 1)}></span>
+        </div>
       </div>
     </section>
   );
--- src/data.js
+++ src/data.js
@@ -2,17 +2,26 @@ const people = [
   {
     id: 1,
     image: '../Images/App_Store_1@4x.png',
+    name: 'maria ferg',
     title: 'The Professional',
+    quote:
+      'Morbi tristique senectus et netus. Pellentesque sit amet porttitor eget dolor morbi non arcu. Sed velit dignissim sodales ut eu sem integer vitae justo. Eget aliquet nibh praesent tristique magna sit. ',
   },
   {
     id: 2,
     image: '../Images/Apple_TV_1@4x.png',
+    name: 'bob ber',
     title: 'The Digital Creator',
+    quote:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam ut porttitor leo a diam sollicitudin. Gravida dictum fusce ut placerat orci nulla pellentesque. Convallis posuere morbi leo urna molestie at. ',
   },
   {
     id: 3,
     image: '../Images/Audible_1@4x.png',
+    name: 'kayte sky',
     title: 'The Performer',
+    quote:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
 ];
 
